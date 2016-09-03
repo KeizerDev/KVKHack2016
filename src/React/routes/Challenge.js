da1fb154ae3c5ec5d522e733c660f17cc8f7a0d5
Daniel Beeke <daniel@studiofonkel.nl>
2016-09-03 12:23:20 +0200

    up

--- src/React/routes/Challenge.js
+++ src/React/routes/Challenge.js
@@ -27,6 +27,10 @@ var Challenge = React.createClass({
             state.data.weeks = [[2,2,2,3,3,2,3], [2,2,2,3,2,2,2], [2,3,2,2,3,3,2], [3,2,3,3,2,2,0], [2,2,3,2,2,2,2], [2,3,3,3,3,3,2], [3,2,3,3,2,1,0], [0,0,0,0,0,0,0], [0,0,0,0,0,0,4]]
         }
 
+        if ($(window).width() / 50 > state.data.weeks.length) {
+            state.scrolled = 0;
+        }
+
         return state;
     },
 
